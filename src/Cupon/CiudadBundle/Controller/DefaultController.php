06a6260682e4303a3ee29d4517e26f7b5ef071f0
Poty Notebook <rivadeneiraie@gmail.com>
2015-04-23 22:31:25 -0300

    Portada finalizada

--- src/Cupon/CiudadBundle/Controller/DefaultController.php
+++ src/Cupon/CiudadBundle/Controller/DefaultController.php
@@ -3,8 +3,26 @@
 namespace Cupon\CiudadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
-    
+	public function cambiarAction($ciudad)
+	{
+		return new RedirectResponse($this->generateUrl('portada', array('ciudad' => $ciudad)));
+	}
+
+	public function listaCiudadesAction($ciudad)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$ciudades = $em->getRepository('CiudadBundle:Ciudad')->findAll();
+		
+		return $this->render('CiudadBundle:Default:listaCiudades.html.twig',
+			array(
+				'ciudadActual' => $ciudad,
+				'ciudades' => $ciudades
+				)
+		);
+	}
+
 }
